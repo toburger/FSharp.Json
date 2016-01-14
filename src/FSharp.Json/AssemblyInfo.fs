0ca34db00298ed945baec680e507b607d3126b04
Tobias Burger <tobias_burger@hotmail.com>
2016-01-14 19:03:06 +0100

    update assembly description

--- src/FSharp.Json/AssemblyInfo.fs
+++ src/FSharp.Json/AssemblyInfo.fs
@@ -3,7 +3,7 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("FSharp.Json")>]
 [<assembly: AssemblyProductAttribute("FSharp.Json")>]
-[<assembly: AssemblyDescriptionAttribute("Elm like Json encoder/decoder combinator library")>]
+[<assembly: AssemblyDescriptionAttribute("Elm inspired Json encoder/decoder combinator library")>]
 [<assembly: AssemblyVersionAttribute("0.0.1")>]
 [<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 do ()
