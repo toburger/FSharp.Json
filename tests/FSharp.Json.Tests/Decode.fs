e18cc16bf22de14a5943ba50f6201e581c52eb76
Tobias Burger <tobias_burger@hotmail.com>
2016-01-14 15:30:40 +0100

    unit test DSL

--- tests/FSharp.Json.Tests/Decode.fs
+++ tests/FSharp.Json.Tests/Decode.fs
@@ -3,140 +3,122 @@ module FSharp.Json.Tests.Decode
 open FSharp.Json.Decode
 open NUnit.Framework
 
-let (|Result|) res =
-    match res with
-    | Ok v -> v
-    | Err err -> failwithf "unexpected: %s" err
+let (==) expected actual =
+    match actual with
+    | Ok v -> Assert.AreEqual(expected, v)
+    | Err err -> Assert.Fail(err)
 
 [<Test>]
 let ``returns "hello world"`` () =
-    let (Result result) = decodeString dstring "\"hello world\""
-    Assert.AreEqual("hello world", result)
+    "hello world" == decodeString dstring "\"hello world\""
 
 [<Test>]
 let ``returns 42`` () =
-  let (Result result) = decodeString dint "42"
-  Assert.AreEqual(42, result)
+  42 == decodeString dint "42"
 
 [<Test>]
 let ``returns 1.23123`` () =
-    let (Result result) = decodeString dfloat "1.23123"
-    Assert.AreEqual(1.23123, result)
+    1.23123 == decodeString dfloat "1.23123"
 
 [<Test>]
 let ``returns true`` () =
-    let (Result result) = decodeString dbool "true"
-    Assert.AreEqual(true, result)
+    true == decodeString dbool "true"
 
 [<Test>]
 let ``returns false`` () =
-    let (Result result) = decodeString dbool "false"
-    Assert.AreEqual(false, result)
+    false == decodeString dbool "false"
 
 [<Test>]
 let ``returns 42 if null`` () =
-    let (Result result) = decodeString (dnull 42) ""
-    Assert.AreEqual(42, result)
+    42 == decodeString (dnull 42) ""
 
 [<Test>]
 let ``returns maybe 42`` () =
-    let (Result result) = decodeString (maybe dint) "42"
-    Assert.AreEqual(Some 42, result)
-    let (Result result) = decodeString (maybe dint) ""
-    Assert.AreEqual(None, result)
+    Some 42 == decodeString (maybe dint) "42"
+    None == decodeString (maybe dint) ""
 
 [<Test>]
 let ``returns object1`` () =
-    let (Result result) =
+    "foo" ==
         decodeString
             (object1 id
                      ("name" := dstring))
             "{ name: \"foo\" }"
-    Assert.AreEqual("foo", result)
 
 [<Test>]
 let ``returns object2`` () =
-    let (Result result) =
+    ("foo", 42) ==
         decodeString
             (object2 (fun name age -> name, age)
                      ("name" := dstring)
                      ("age" := dint))
             "{ name: \"foo\", age: 42 }"
-    Assert.AreEqual(("foo", 42), result)
 
 [<Test>]
 let ``returns integer list`` () =
-    let (Result result) =
+    [1..10] ==
         decodeString
             (list dint)
             "[ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]"
-    Assert.AreEqual([1..10], result)
 
 [<Test>]
 let ``returns either foo or bar if empty`` () =
     let test input =
-        let (Result result) =
-            decodeString
-                (oneOf [dstring; dnull "bar"])
-                input
-        result
-    Assert.AreEqual("foo", test "\"foo\"")
-    Assert.AreEqual("bar", test "\"bar\"")
-    Assert.AreEqual("bar", test "")
+        decodeString
+            (oneOf [dstring; dnull "bar"])
+            input
+    "foo" == test "\"foo\""
+    "bar" == test "\"bar\""
+    "bar" == test ""
 
 [<Test>]
 let ``returns keyvaluepairs`` () =
-    let (Result result) =
+    [ ("name", "foo"); ("name2", "bar") ] ==
         decodeString
             (keyValuePairs dstring)
             "{ name: \"foo\", name2: \"bar\" }"
-    Assert.AreEqual([ ("name", "foo"); ("name2", "bar") ], result)
 
 [<Test>]
 let ``returns map`` () =
-    let (Result result) =
+    Map.ofList [ ("name", "foo"); ("name2", "bar") ] ==
         decodeString
             (dmap dstring)
             "{ name: \"foo\", name2: \"bar\" }"
-    Assert.AreEqual(Map.ofList [ ("name", "foo"); ("name2", "bar") ], result)
 
 [<Test>]
 let ``returns email and age at position`` () =
     let json = "{ person: { contact: { email: \"email@example.com\" }, age: 42 } }"
-    let (Result email) =
+    let email =
         decodeString
             (at [ "person"; "contact"; "email" ] dstring)
             json
-    Assert.AreEqual("email@example.com", email)
-    let (Result age) =
+    "email@example.com" == email
+    let age =
         decodeString
             (at [ "person"; "age" ] dint)
             json
-    Assert.AreEqual(42, age)
+    42 == age
 
 [<Test>]
 let ``returns tuple (foo, 42)`` () =
-    let (Result result) =
+    ("foo", 42) ==
         decodeString
             (tuple2 (fun s i -> s, i) dstring dint)
             "[\"foo\", 42]"
-    Assert.AreEqual(("foo", 42), result)
 
 [<Test>]
 let ``returns tuple (foo, 42, baz)`` () =
-    let (Result result) =
+    ("foo", 42, "baz") ==
         decodeString
             (tuple3 (fun s i s2 -> s, i, s2) dstring dint dstring)
             "[\"foo\", 42, \"baz\"]"
-    Assert.AreEqual(("foo", 42, "baz"), result)
 
 [<Test>]
 let ``returns tuple (foo, 42, baz, false)`` () =
-    let (Result result) =
+    ("foo", 42, "baz", false) ==
         decodeString
             (tuple4 (fun s i s2 b -> s, i, s2, b) dstring dint dstring dbool)
             "[\"foo\", 42, \"baz\", false]"
-    Assert.AreEqual(("foo", 42, "baz", false), result)
 
 [<Test>]
 let ``returns crazy formatted data`` () =
@@ -152,9 +134,8 @@ let ``returns crazy formatted data`` () =
                     (decodeValue b two)
                     rest'
             | _ -> Err "expecting at least two elements in array")
-    let (Result result) =
+    (false, "test", [ 42; 12; 12 ]) ==
         decodeString
             (variadic2 (fun a b c -> a, b, c) dbool dstring dint)
             "[false, \"test\", 42, 12, 12]"
-    Assert.AreEqual((false, "test", [ 42; 12; 12 ]), result)
 
