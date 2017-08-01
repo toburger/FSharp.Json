afbc54eae5f957f38bc70573100a4af4761035ae
Tobias Burger <tobias_burger@hotmail.com>
2017-08-01 13:49:59 +0200

    release new beta: 0.0.2

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
 #### 0.0.1-beta - May 24 2017
 * Initial release
+
+### 0.0.2-beta - Aug 01 2017
+* Better error message for field deserialization
--- src/FSharp.Json/AssemblyInfo.fs
+++ src/FSharp.Json/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Json")>]
 [<assembly: AssemblyProductAttribute("FSharp.Json")>]
 [<assembly: AssemblyDescriptionAttribute("Elm inspired Json encoder/decoder combinator library")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSharp.Json"
     let [<Literal>] AssemblyProduct = "FSharp.Json"
     let [<Literal>] AssemblyDescription = "Elm inspired Json encoder/decoder combinator library"
-    let [<Literal>] AssemblyVersion = "0.0.1"
-    let [<Literal>] AssemblyFileVersion = "0.0.1"
+    let [<Literal>] AssemblyVersion = "0.0.2"
+    let [<Literal>] AssemblyFileVersion = "0.0.2"
